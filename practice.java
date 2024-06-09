734fac6af36deaabc906c23060f1982d721d9fbe
Prashanth837482 <prashanthvangapelli369@gmail.com>
2024-06-09 11:58:46 +0530

    Update practice.java

--- practice.java
+++ practice.java
@@ -75,7 +75,7 @@ public class practice{
 			System.out.println("false");
 		}
 	}
-	
+	// https://www.geeksforgeeks.org/problems/sum-of-all-divisors-from-1-to-n4738/1
 	public void sumOfDivisorsAndMore(int num) {
 		
 		int sum=0;
